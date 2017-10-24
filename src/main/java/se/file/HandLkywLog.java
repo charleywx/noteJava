1bfc7d96db63b4ee8d17a215ddb51ff00e63c0e8
charley <shenbing1201@163.com>
2017-10-24 16:30:08 +0800

    0

---
+++ src/main/java/se/file/HandLkywLog.java
@@ -0,0 +1,22 @@
+package se.file;
+
+
+import util.FileUtil;
+import util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HandLkywLog {
+    public static void main(String[] args) {
+        List<String> list = FileUtil.readFileByLineToList("C:\\Users\\shenb\\Desktop\\LkywGps.log");
+        Set<String> set = new HashSet<>();
+        for(String s: list) {
+            if(StringUtil.isBlank(s)) continue;
+            set.add(s.split(",")[7] + ",");
+        }
+        FileUtil.writeFile("C:\\Users\\shenb\\Desktop\\carIds.txt", new ArrayList(set), false);
+    }
+}
